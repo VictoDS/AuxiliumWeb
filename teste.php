7a30e1fa7c85f445efd4981f3ba955200086fb4e
VictoDS <victor.scandolara@gmail.com>
2017-07-29 11:57:20 -0300

    update de teste

--- teste.php
+++ teste.php
@@ -3,19 +3,6 @@
 		<title>Teste Requisição</title>
 	</head>
 	<body>
-		<button onclick="encerraSol(1);">Testar</button><br>
-		<div id="ctrlSolicitacao"></div>
-		<script>
-			function encerraSol(idOcorencia){
-				$.get("http://seleman.16mb.com/statusSolicitacao.php",
-					{
-						id  : idOcorencia,
-						status: 'E'
-					},
-				function(data, status){
-					$('#ctrlSolicitacao').html(data);
-				});
-			}
-		</script>
+		<h1>Sem PHP</h1>
 	</body>
 </html>
